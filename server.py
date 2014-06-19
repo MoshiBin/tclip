1fdaaefa6c4eaf03ecf3f08317e0039c40fa020f
Moshi Binyamini <moshi.binyamini@emc.com>
2014-06-19 17:19:32 +0300

    Actually run the server

--- server.py
+++ server.py
@@ -18,3 +18,7 @@ def set_clip():
     SetClipboardText(request.form["content"])
     CloseClipboard()
     return "OK"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=80)
